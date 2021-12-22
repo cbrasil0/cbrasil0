038e49635c27228e9c39520c0784771f75b61c1b
cbrasil0 <93555591+cbrasil0@users.noreply.github.com>
2021-12-22 18:50:35 -0300

    Update caio.py

--- caio.py
+++ caio.py
@@ -1,4 +1,5 @@
-#resumo de minha trajetória acadêmico-profissional até então
+#olá!
+#este é um resumo de minha trajetória acadêmico-profissional até então:
 
 from datetime import date
 Caio = () #começo
@@ -9,10 +10,10 @@ while career == "Estudante":
         career = "Bacharel"
         oab = True #aprovação no exame da OAB na 1ª tentativa, ainda acadêmico de Direito
         if oab:
-            career = "Advogado" #por razões financeiras, apenas emiti a carteira profissional em ago/2017, tornando-me advogado
+            career = "Advogado" #por razões financeiras, apenas emiti a carteira profissional em ago/2017, tornando-me advogado com atuação majoritária na área Cível
             pos_g = True
             if pos_g:
-                career += " Especializado" #com início em nov/2018, concluí minha Pós-graduação em Responsabilidade Civil e Direito do Consumidor em 2020
+                career += " Especializado" #Pós-graduação em Responsabilidade Civil e Direito do Consumidor iniciada em 2018 e concluída em 2020
                 def nowadays(year):
                     if year >= 2021:
                         return (career + "\n&\nDesenvolvedor") #desde 2021, graduando em Sistemas de Informação e desenvolvedor
@@ -20,4 +21,4 @@ data = date.today()
 Caio = nowadays(data.year)
 print(Caio + "\n\nVamos conversar?") # buscando oportunidades profissionais nível Júnior e colaboração em projetos em geral, sempre aberto a novas ideias e sugestões
 
-#obrigado pela atenção
+#obrigado pela atenção!
