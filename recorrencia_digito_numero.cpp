cc6c9c7e2b41a19016886ff762ea4a759c9746e1
cbrasil0 <93555591+cbrasil0@users.noreply.github.com>
2022-01-28 15:01:45 -0300

    Update recorrencia_digito_numero.cpp

--- recorrencia_digito_numero.cpp
+++ recorrencia_digito_numero.cpp
@@ -2,16 +2,13 @@
 
 // A função abaixo recebe o dígito k e o número n, os compara e encontra quantas vezes k se repete em n.
 int recorrencia(int n, int k) {
-int recorrencia(int n, int k) {
     int x = 0; // contador de repetições
     if (n == 0) {
         return 0;
     }
-    if (n > 0) { // loop recursivo
-        int digito = n % 10; // resto = último dígito do decimal n
-        if (digito == k) { // condicional: dígito em n = k - primeiro caso de comparação positiva
-            return 1 + recorrencia(n/10,k);// recursão à própria função
-        }
+    int digito = n % 10; // resto = último dígito do decimal n
+    if (digito == k) { // condicional: dígito em n = k -> comparação positiva
+        return 1 + recorrencia(n/10,k);// incremento + recursão à própria função
     }
     return recorrencia(n/10,k); // função retorna o número de repetições de k em n
 }
