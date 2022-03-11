031fe0b6b9719940e81b0bd2a2c1e532f5453c04
Caio Brasil <caiobrasilraposo@gmail.com>
2022-03-11 16:51:45 -0300

    Add files via upload

---
+++ projects/Python3/conversor_temperatura.py
@@ -0,0 +1,55 @@
+'''
+
+Bem-vindo ao conversor de medidas de temperatura do Caio!
+
+'''
+def CtoK(temperatura):
+    K = temperatura + 273.15
+    print()
+    print(temperatura,"º Celsius equivalem a ",f"{K:.3f}","º Kelvin.",sep='')
+    return 0
+def CtoF(temperatura):
+    F = temperatura * 1.8 + 32
+    print()
+    print(temperatura,"º Celsius equivalem a ",f"{F:.3f}","º Farenheit.",sep='')
+    return 0
+def KtoC(temperatura):
+    C = temperatura - 273.15
+    print()
+    print(temperatura,"º Kelvin equivalem a ",f"{C:.3f}","º Celsius.",sep='')
+    return 0
+def KtoF(temperatura):
+    F = (temperatura - 273.15) * 1.8 + 32
+    print()
+    print(temperatura,"º Kelvin equivalem a ",f"{F:.3f}","º Farenheit.",sep='')
+    return 0
+def FtoC(temperatura):
+    C = (temperatura - 32) * 5 / 9
+    print()
+    print(temperatura,"º Farenheit equivalem a ",f"{C:.3f}","º Celsius.",sep='')
+    return 0
+def FtoK(temperatura):
+    K = (temperatura - 32) * 5 / 9 + 273.15
+    print()
+    print(temperatura,"º Farenheit equivalem a ",f"{K:.3f}","º Kelvin.",sep='')
+    return 0
+medida = int(input("Insira o dígito de acordo com a opção que a temperatura está, sendo 1 - Celsius, 2 - Kelvin ou 3 - Farenheit: "))
+nova_medida = int(input("Insira o dígito de acordo com a medida desejada de temperatura, sendo 1 - Celsius, 2 - Kelvin ou 3 - Farenheit: "))
+temperatura = float(input("Insira o número de graus da temperatura: "))
+if medida == nova_medida:
+    print("Erro, conversão para a mesma medida. Tente novamente.")
+else:
+    if (medida == 1) and (nova_medida == 2):
+        CtoK(temperatura)
+    elif (medida == 1) and (nova_medida == 3):
+        CtoF(temperatura)
+    elif (medida == 2) and (nova_medida == 1):
+        KtoC(temperatura)
+    elif (medida == 2) and (nova_medida == 3):
+        KtoF(temperatura)
+    elif (medida == 3) and (nova_medida == 1):
+        FtoC(temperatura)
+    elif (medida == 3) and (nova_medida == 2):
+        FtoK(temperatura)
+    else:
+        print("Opção inválida. Tente novamente.")
